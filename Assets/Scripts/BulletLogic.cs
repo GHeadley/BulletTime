2f0c80c5f865ee245e6bde93f0b10c5b3fda731d
Gunnar Headley <johngheadley@gmail.com>
2020-12-05 16:18:46 -0800

    Adding Gun firing logic

---
+++ Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed = 1;
+    public float TTL = 30;
+
+    private Vector3 direction;
+
+    void Start()
+    {
+        //direction = Vector3.forward;
+        Destroy(this.gameObject, TTL);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(-this.transform.forward * Time.deltaTime * Speed);
+    }
+}
---
+++ Assets/Scripts/GunLogic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLogic : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject BulletPrefab;
+    public GameObject BulletSpawn;
+    public float AimDuration;
+
+    private float currDuration = 0;
+    private Vector3 playerLocation;
+    private Vector3 startGunLocation;
+
+    public GunLogic(GameObject player, GameObject bulletPrefab, float aimDuration, GameObject bulletSpawn)
+    {
+        this.Player = player;
+        this.BulletPrefab = bulletPrefab;
+        this.AimDuration = aimDuration;
+        this.BulletSpawn = bulletSpawn;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLocation = Player.transform.position;
+        startGunLocation = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currDuration += Time.deltaTime;
+        if(currDuration >= AimDuration)
+        {
+            FireGun();
+            Destroy(this.gameObject);
+        }
+        float currY = Mathf.Lerp(startGunLocation.y, playerLocation.y, currDuration / AimDuration);
+        Vector3 newLocation = new Vector3(startGunLocation.x, currY, startGunLocation.z);
+        this.transform.position = newLocation;
+        this.transform.LookAt(Player.transform, Vector3.up);
+    }
+
+    private void FireGun()
+    {
+        Debug.Log("Position: " + BulletSpawn.transform.position);
+        Debug.Log("Rotation: " + this.transform.rotation);
+        Instantiate(BulletPrefab, BulletSpawn.transform.position, this.transform.rotation);
+        Debug.Log("Fired Gun!");
+    }
+}
