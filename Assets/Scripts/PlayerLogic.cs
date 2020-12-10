58454d267bc41c0b58e10d5b8861a528dbe0fd74
Gunnar Headley <johngheadley@gmail.com>
2020-12-09 21:01:13 -0800

    Adding Player Health

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public int PlayerHealth;
+    public string ExpectedBulletTag;
+
+    private SphereCollider Collider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(ExpectedBulletTag))
+        {
+            if(--PlayerHealth == 0)
+            {
+                PlayerDeath();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider = this.gameObject.GetComponent<SphereCollider>();
+    }
+
+    void PlayerDeath()
+    {
+        Debug.Log("Player Died!");
+    }
+}
